05c164cb57493d453cfb5426f15dc1e606f9a235
Yan Krug Gerhardt <yan-ragna@hotmail.com>
2025-04-14 08:18:34 -0300

    update user address schema to have first and last name

--- src/schema/address.schema.ts
+++ src/schema/address.schema.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 export const UserAddressSchema = z.object({
+  firstName: z.string().min(2),
+  lastName: z.string().min(2),
   zip: z.string().min(5),
   address1: z.string().min(3),
   address2: z.string().optional(),
--- src/types/address.d.ts
+++ src/types/address.d.ts
@@ -1,6 +1,8 @@
 export interface UserAddress {
   userId?: string;
   id: string;
+  firstName: string;
+  lastName: string;
   zip: string;
   address1: string;
   address2?: string;
